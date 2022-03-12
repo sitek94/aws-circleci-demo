33565d3dd0e9c19dedac40bad0d02fd2b1664f9d
Maciek <msitkowski94@gmail.com>
2022-03-12 16:52:09 +0100

    prepare v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { useState } from 'react'
+import pkg from '../package.json'
 import logo from './logo.svg'
 import './App.css'
 
+import * as React from 'react'
+import GithubCorner from '@uiw/react-github-corners'
+
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = React.useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
+      <GithubCorner
+        target="_blank"
+        href={pkg.repository.url}
+        color="var(--color-gray)"
+        bgColor="var(--color-blue)"
+        position="right"
+      />
+      <main>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <h1 data-cy="app-title">
+          {pkg.name} v{pkg.version}
+        </h1>
         <p>
-          <button type="button" onClick={() => setCount(count => count + 1)}>
+          <button
+            type="button"
+            onClick={() => setCount(count => count + 1)}
+            data-cy="counter-btn"
+          >
             count is: {count}
           </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
           <a
@@ -36,7 +50,7 @@ export function App() {
             Vite Docs
           </a>
         </p>
-      </header>
+      </main>
     </div>
   )
 }
