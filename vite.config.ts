991663121c5c5577c3180ee3f7af4cfd9e0c4bd5
Maciek <msitkowski94@gmail.com>
2022-03-13 06:58:03 +0100

    report coverage after test job

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,8 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    coverage: {
+      reporter: ['json', 'html'],
+    },
   },
 })
